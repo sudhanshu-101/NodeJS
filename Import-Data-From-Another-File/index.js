f6291fca55e048e6c2ec41e21de02db34a8876ed
Sudhanshu Kumar <sudhanshu95084145@gmail.com>
2025-05-25 13:05:53 +0530

    Add files via upload

---
+++ Import-Data-From-Another-File/index.js
@@ -0,0 +1,10 @@
+// This file import the data of another file.
+
+
+import { sum, mul, PI, g } from "./math.js";
+
+console.log(`The value of the PI = ${PI}`);
+console.log(`The value of gravity = ${g}`)
+console.log(`The sum of (23,34) number are = ${sum(24, 34)}`);
+console.log(`The multiplication of (12,23) numbers are = ${mul(12, 23)}`);
+
---
+++ Import-Data-From-Another-File/math.js
@@ -0,0 +1,26 @@
+// This file export the data from this file to another file
+
+// // Method 1 to export the data from one file to another file
+
+// const PI = 3.14;
+// const g = 9.8;
+
+// const sum = (a, b) => a + b;
+// const mul = (a, b) => a * b;
+
+// module.exports = {
+//     PI:PI,
+//     g:g,
+//     sum:sum,
+//     mul:mul
+// };
+
+
+// Method 2 to export the data from one file to another file
+
+export const PI = 3.14;
+export const g = 9.8;
+
+export const sum = (a, b) => a + b;
+export const mul = (a, b) => a * b;
+
