0437a0ff72fd6b1588a73bd251b3e4b7c9415b45
Sudhanshu Kumar <sudhanshu95084145@gmail.com>
2025-05-25 14:07:39 +0530

    Update index.js

--- Import-Data-From-Another-File/index.js
+++ Import-Data-From-Another-File/index.js
@@ -1,10 +1,26 @@
-// This file import the data of another file.
+// This file import the data of another file math.js
 
 
-import { sum, mul, PI, g } from "./math.js";
+// Method 1
 
-console.log(`The value of the PI = ${PI}`);
-console.log(`The value of gravity = ${g}`)
-console.log(`The sum of (23,34) number are = ${sum(24, 34)}`);
-console.log(`The multiplication of (12,23) numbers are = ${mul(12, 23)}`);
+        import { sum, mul, PI, g } from "./math.js";
+
+        console.log(`The value of the PI = ${PI}`);
+        console.log(`The value of gravity = ${g}`)
+        console.log(`The sum of (23,34) number are = ${sum(24, 34)}`);
+        console.log(`The multiplication of (12,23) numbers are = ${mul(12, 23)}`);
+
+
+
+
+
+// Method 2
+
+        // import { sum, mul, PI, g } from "./math.js";
+
+        // console.log(`The value of the PI = ${PI}`);
+        // console.log(`The value of gravity = ${g}`)
+        // console.log(`The sum of (23,34) number are = ${sum(24, 34)}`);
+        // console.log(`The multiplication of (12,23) numbers are = ${mul(12, 23)}`);
+
 
